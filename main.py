f39e5a6aa442ac2d7e2fac2da6a1187ee6787748
Redhawk18 <Redhawk76767676@gmail.com>
2022-02-02 16:17:05 -0500

    refactored classes to work, and moved around code to fix other things

--- main.py
+++ main.py
@@ -1,30 +1,17 @@
 # created 1/17/2022
 # this file should create objects and append them into a longer string
-from fileinput import close
 import random
 from words import *
 
-def readTextfile(filename):
-    fileOutput = []
-    for i in open(filename, "r"):
-        line = i.strip().lower()
-        fileOutput.append(line)
-    close()
-    return fileOutput
-
-#most basic sentence
-
-#TODO make objects to make the words more print statements readable and to learn about python classes
-#TODO add some kind of random so different sentences
 amountOfSentenceTypes = 1
 
 if random.randrange(amountOfSentenceTypes) == 0:
     #sentence type 1
-    #The adjective noun.
-    n = noun(readTextfile("adjective.txt"))
-    #print("The", random.choice(readTextfile("adjective.txt")), random.choice(readTextfile("noun.txt")) +".")
-    print(n)
+    #"The" adjective noun.
+    
+    n = noun("noun.txt")
+    a = adjective("adjective.txt")
+    print("The", n.print(), a.print() +".")
+    
 else: 
-    print("no")
-
-    #test from desktop
+    print("amountOfSentenceTypes is wrong")
--- words.py
+++ words.py
@@ -1,22 +1,49 @@
 from abc import abstractclassmethod
+from fileinput import close
+import random
 
+def readTextfile(filename):
+    '''Takes a file name and creates a list and output's file content to list in all lowercase'''
+    fileOutput = []
+    for i in open(filename, "r"):
+        line = i.strip().lower()
+        fileOutput.append(line)
+    close()
+    return fileOutput
 
 class word():
     @abstractclassmethod
-    def word(self):
-        pass
+
+    def __init__(self, fileName):
+        self.wordlist = readTextfile(fileName)
+
+    def __len__(self):
+        return len(self.wordlist)
+
+    def print(self):
+        '''this function returns a random word from the list passed into the object'''
+        return random.choice(self.wordlist)
+
+    def printSelfAndParents(self, wordlist):
+        '''makes a new list that has the strings from the wordlist and the object parent's wordlist and returns one of them'''
+        wordListWithParents = ()
+        
+        for i in wordlist: #current object's list
+            line = wordlist[i]
+            wordListWithParents.append(line)
+        
+        for j in super.wordlist: #parent's list
+            line = super.wordlist[j]
+            wordListWithParents.append(line)
+
+        #both lists are in the same new list now, and there's no need to sort
+        return random.choice(self.wordListWithParents)
 
 class adjective(word):
-
-    def word(self):
-        print("adjective")
+    pass
 
 class verb(word):
-
-    def word(self):
-        print("verb")
+    pass
 
 class noun(word):
-    #for subclasses of noun make it so you have word() and wordAndParent() which turns a random word from it's own list and it's parents
-    def word(self):
-        print("noun")
+    pass
