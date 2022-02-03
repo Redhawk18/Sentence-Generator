456f37dfef42cf3fae2f714a6b0b25d7b0f916bd
Redhawk18 <Redhawk76767676@gmail.com>
2022-02-02 21:39:55 -0500

    improved paths and folders

--- main.py
+++ main.py
@@ -1,15 +1,19 @@
 # created 1/17/2022
 # this file should create objects and append them into a longer string
 import random
+from pathlib import Path
+
 from words import *
+
+wordlists = Path("wordlists")
 
 amountOfSentenceTypes = 1
 rand = random.randrange(amountOfSentenceTypes)
 
 if rand == 0:
     #"The" adjective noun.
-    n = noun("noun.txt")
-    a = adjective("adjective.txt")
+    n = noun(wordlists/"noun.txt")
+    a = adjective(wordlists/"adjective.txt")
     print("The", a.__str__(), n.__str__() +".")
     
 else: 
