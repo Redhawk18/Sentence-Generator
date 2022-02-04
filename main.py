98101b663642aa75f421f4f14255c664c0248f9e
Redhawk18 <Redhawk76767676@gmail.com>
2022-02-03 19:11:59 -0500

    start of command line input

--- main.py
+++ main.py
@@ -1,20 +1,28 @@
 # created 1/17/2022
 # this file should create objects and append them into a longer string
+from cmath import pi
 import random
 from pathlib import Path
-
+import sys
 from words import *
 
-wordlists = Path("wordlists")
+def pickRandomSentence():
+    wordlists = Path("wordlists")
 
-amountOfSentenceTypes = 1
-rand = random.randrange(amountOfSentenceTypes)
+    amountOfSentenceTypes = 1
+    rand = random.randrange(amountOfSentenceTypes)
 
-if rand == 0:
-    #"The" adjective noun.
-    n = noun(wordlists/"noun.txt")
-    a = adjective(wordlists/"adjective.txt")
-    print("The", a.randomize_word(), n.randomize_word() +".")
-    
-else: 
-    print("amountOfSentenceTypes is wrong")
+    if rand == 0:
+        #"The" adjective noun.
+        n = noun(wordlists/"noun.txt")
+        a = adjective(wordlists/"adjective.txt")
+        print("The", a.randomizeWord(), n.randomizeWord() +".")
+        
+    else: 
+        print("amountOfSentenceTypes is wrong")
+
+
+#user input
+for arg in sys.argv:
+    print(arg)
+
