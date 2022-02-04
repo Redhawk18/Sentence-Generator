edffb53e68b872ea3024e837dfd55d8c2a7e7406
Redhawk18 <Redhawk76767676@gmail.com>
2022-02-03 19:47:47 -0500

    working user input with unix-like flags

--- main.py
+++ main.py
@@ -1,12 +1,14 @@
 # created 1/17/2022
 # this file should create objects and append them into a longer string
 from cmath import pi
+from operator import truediv
 import random
 from pathlib import Path
+from re import L
 import sys
 from words import *
 
-def pickRandomSentence():
+def pickAndPrintRandomSentence():
     wordlists = Path("wordlists")
 
     amountOfSentenceTypes = 1
@@ -23,6 +25,33 @@ def pickRandomSentence():
 
 
 #user input
+loopControl = 1 #these have to be passed to the function no matter what, even without input
+loopBoolean = False
+RandomSentence = True
+
 for arg in sys.argv:
-    print(arg)
+    print("DEBUG: args", arg)
+    if(arg == "-help" or arg == "--help" or arg == "-h"):
+        '''prints a list of commands with descriptions of their outputs'''
+        print()
+        print("-h or -help")
+        print("\tprints a list of commands with descriptions of their outputs\n")
+        print("-l or -loop")
+        print("\ttakes value after and will loop input-th amount of times, will fail if input is below 0\n")
+        print("-p or -phase")
+        print("\tdisables the randomimze for sentence phases\n")
+
+    elif(arg == "-l" or arg == arg == "-loop"):
+        '''takes integer value after and will loop input-th amount of times, will fail if input is below 0'''
+        loopBoolean = True
+
+    elif(loopBoolean):
+        loopControl = arg
+        loopBoolean = False
+
+    elif(arg == "-p" or arg == arg == "-phase"):
+        '''disables the randomimze for sentence phases'''
+        RandomSentence = False
+
+
 
