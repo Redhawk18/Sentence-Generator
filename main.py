7351b48422c655952b793b10988b2c590575ad6e
Redhawk18 <Redhawk76767676@gmail.com>
2022-02-03 20:18:25 -0500

    added functionality to the user input

--- main.py
+++ main.py
@@ -8,32 +8,33 @@ from re import L
 import sys
 from words import *
 
-def pickAndPrintRandomSentence(loopControl, RandomSentence):
+def pickAndPrintRandomSentence(loopControl, randomSentence):
     wordlists = Path("wordlists")
 
     amountOfSentenceTypes = 1
     #it's easier to have the same random in two places because of the RandomSentence boolean
     rand = random.randrange(amountOfSentenceTypes)
 
-    #for i in range(0, loopControl):
-        
-    if rand == 0:
-        #"The" adjective noun.
-        n = noun(wordlists/"noun.txt")
-        a = adjective(wordlists/"adjective.txt")
-        print("The", a.randomizeWord(), n.randomizeWord() +".")
-        
-    else: 
-        print("amountOfSentenceTypes is wrong")
+    for i in range(0, loopControl):    
+        if(randomSentence == True):
+            rand = random.randrange(amountOfSentenceTypes)
+
+        if rand == 0:
+            #"The" adjective noun.
+            n = noun(wordlists/"noun.txt")
+            a = adjective(wordlists/"adjective.txt")
+            print("The", a.randomizeWord(), n.randomizeWord() +".")
+                
+        else: 
+            print("something is wrong...")
 
 
 #user input
 loopControl = 1 #these have to be passed to the function no matter what, even without input
-RandomSentence = True
+randomSentence = True
 
 loopBoolean = False
 for arg in sys.argv:
-    print("DEBUG: args", arg)
     if(arg == "-help" or arg == "--help" or arg == "-h"):
         '''prints a list of commands with descriptions of their outputs'''
         print()
@@ -49,21 +50,19 @@ for arg in sys.argv:
         loopBoolean = True
 
     elif(loopBoolean): 
-        #messy, but collects the value after the flag is called
-        print(loopControl)
+        #messy code, but collects the value after the flag is called
         loopControl = int(arg)
-        
         loopBoolean = False #so the value isn't overwritten
 
         #check loopControl for value integer
-        print(loopControl)
         if(loopControl < 1):
             print("Invalid loop input ")
             sys.exit()
 
     elif(arg == "-p" or arg == arg == "-phase"):
         '''disables the randomimze for sentence phases'''
-        RandomSentence = False
+        randomSentence = False
 print("loopControl", loopControl)
 
 
+pickAndPrintRandomSentence(loopControl, randomSentence)
